e3947822df0c50fc2ecb8afbaa128fbfce79da63
JohnVonNeumann <louiswillcock@gmail.com>
2023-12-24 15:17:38 +1100

    CREATE test_user_model.py - test user model

---
+++ tests/test_models/test_user_model.py
@@ -0,0 +1,23 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+
+from db.models.users import User
+
+
+def test_user_creation(test_user):
+    assert test_user.id is not None
+    assert test_user.username == "testuser"
+    assert test_user.email == "testuser@example.com"
+    assert test_user.is_active is True
+    assert test_user.is_superuser is False
+
+
+def test_user_uniqueness(db_session):
+    user1 = User(username="user1", email="user1@example.com", hashed_password="password")
+    db_session.add(user1)
+    db_session.commit()
+
+    with pytest.raises(IntegrityError):
+        user2 = User(username="user1", email="user2@example.com", hashed_password="password")
+        db_session.add(user2)
+        db_session.commit()
