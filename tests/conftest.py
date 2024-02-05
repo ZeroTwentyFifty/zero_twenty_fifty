4573c6409ae100e8e8d4925ed115595a7f5ee269
JohnVonNeumann <louiswillcock@gmail.com>
2024-02-06 10:10:03 +1100

    CREATE conftest.py - add pytest conftest for shared fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,97 @@
+import os
+import sys
+from typing import Any
+from typing import Generator
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# this is to include backend dir in sys.path so that we can import from db,main.py
+
+from db.base import Base
+from db.session import get_db
+from db.repository.users import create_new_user
+from apis.base import api_router
+from schemas.user import UserCreate
+
+
+def start_application():
+    app = FastAPI()
+    app.include_router(api_router)
+    return app
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+# Use connect_args parameter only with sqlite
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="module")
+def app() -> Generator[FastAPI, Any, None]:
+    """
+    Create a fresh database on each test case.
+    """
+    Base.metadata.create_all(engine)
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture(scope="module")
+def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session  # use the session in tests.
+    session.close()
+    if transaction.is_active:
+        transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="module")
+def client(
+    app: FastAPI, db_session: SessionTesting
+) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="module")
+def test_user(db_session: SessionTesting):
+    user_data = {
+        "username": "testuser",
+        "email": "testuser@example.com",
+        "password": "testuser",
+    }
+    user = create_new_user(user=UserCreate(**user_data), db=db_session)
+    return user
+
+
+"""
+TODO: Consolidate and clean up the fixtures across the tests, if there are fixtures that
+are duplicates across files, look into putting them in one place and having the system
+set itself up easily. A good place to start would probably be just learning a bit more
+about good quality fixture organisation in Pytest.
+"""
+
