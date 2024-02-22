8eadf654505d7d674af19fc21814dcc5a8786381
JohnVonNeumann <louiswillcock@gmail.com>
2024-02-22 15:56:54 +1100

    UPDATE alembic - utilise variable interpolation for database variables

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,12 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from db.base import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,14 +19,14 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+section = config.config_ini_section
+config.set_section_option(section, "DATABASE_URL", os.environ.get("DATABASE_URL"))
 
 
 def run_migrations_offline() -> None:
