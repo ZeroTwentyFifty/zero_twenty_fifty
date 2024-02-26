e487911049565ff736ff3db7b4048b41e154f25d
JohnVonNeumann <louiswillcock@gmail.com>
2024-02-26 15:26:44 +1100

    UPDATE CarbonFootprint entity - completely rebuild the CarbonFootprint schema
    Rather large update in one go, but I don't like shipping code that can
    break builds, so it all came through in one update. This update basically
    brings the CarbonFootprint entity into much closer alignment with the
    actual specification, one step on the way to adding an actual db table
    to support non-json data in the database table

--- schemas/carbon_footprint.py
+++ schemas/carbon_footprint.py
@@ -1,30 +1,259 @@
-from datetime import datetime, timezone
-from typing import List
+from __future__ import annotations
+from enum import Enum
+from typing import Any, Dict, List, Optional
 
-from pydantic import field_validator, BaseModel, Field
+from pydantic import (
+    AwareDatetime, BaseModel, ConfigDict, Field, PositiveFloat, confloat, constr,
+    field_serializer
+)
+
+
+class CharacterizationFactors(str, Enum):
+    """
+    The IPCC version of the GWP characterization factors used in the calculation of the PCF (see Pathfinder Framework Section 3.2.2).
+    The value MUST be one of the following:
+
+    - AR6: for the Sixth Assessment Report of the Intergovernmental Panel on Climate Change (IPCC)
+    - AR5: for the Fifth Assessment Report of the IPCC.
+
+    The set of characterization factor identifiers will likely change in future revisions.
+    It is recommended to account for this when implementing the validation of this property.
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#element-attrdef-carbonfootprint-characterizationfactors
+    """
+    AR5 = 'AR5'
+    AR6 = 'AR6'
+
+
+class BiogenicAccountingMethodology(str, Enum):
+    """
+    The standard followed to account for biogenic emissions and removals.
+    If defined, the value MUST be one of the following:
+
+    - PEF: For the EU Product Environmental Footprint Guide
+    - ISO: For the ISO 14067 standard
+    - GHGP: For the Greenhouse Gas Protocol (GHGP) Land sector and Removals Guidance
+    - Quantis: For the Quantis Accounting for Natural Climate Solutions Guidance
+
+    The enumeration of standards above will be evolved in future revisions.
+    Account for this when implementing the validation of this property.
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#element-attrdef-carbonfootprint-biogenicaccountingmethodology
+    """
+    PEF = 'PEF'
+    ISO = 'ISO'
+    GHGP = 'GHGP'
+    QUANTIS = 'Quantis'
+
+
+class DeclaredUnit(str, Enum):
+    """
+    DeclaredUnit is the enumeration of accepted declared units with values.
+
+    The values are based on the International System of Units (SI) as defined by the International Committee for Weights and Measures (ICWM).
+
+    - LITER: for special SI Unit litre
+    - KILOGRAM: for SI Base Unit kilogram
+    - CUBIC_METER: for cubic meter, the Derived Unit from SI Base Unit metre
+    - KILOWATT_HOUR: for kilowatt hour, the Derived Unit from special SI Unit watt
+    - MEGAJOULE: for megajoule, the Derived Unit from special SI Unit joule
+    - TON_KILOMETER: for ton kilometer, the Derived Unit from SI Base Units kilogram and metre
+    - SQUARE_METER: for square meter, the Derived Unit from SI Base Unit metre
+
+    The value of each DeclaredUnit MUST be encoded as a JSON String.
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#dt-declaredunit
+    And the SI Unit reference:
+        https://www.bipm.org/documents/20126/41483022/SI-Brochure-9-EN.pdf/2d2b50bf-f2b4-9661-f402-5f9d66e4b507
+    """
+    LITER = 'liter'
+    KILOGRAM = 'kilogram'
+    CUBIC_METER = 'cubic meter'
+    KILOWATT_HOUR = 'kilowatt hour'
+    MEGAJOULE = 'megajoule'
+    TON_KILOMETER = 'ton kilometer'
+    SQUARE_METER = 'square meter'
+
+
+class RegionOrSubregion(str, Enum):
+    """The RegionOrSubregion data type represents a geographic region or subregion.
+
+    It MUST be encoded as a String with value equal to one of the following values:
+
+    - Africa: for the UN geographic region Africa
+    - Americas: for the UN geographic region Americas
+    - Asia: for the UN geographic region Asia
+    - Europe: for the UN geographic region Europe
+    - Oceania: for the UN geographic region Oceania
+    - Australia and New Zealand: for the UN geographic subregion Australia and New Zealand
+    - Central Asia: for the UN geographic subregion Central Asia
+    - Eastern Asia: for the UN geographic subregion Eastern Asia
+    - Eastern Europe: for the UN geographic subregion Eastern Europe
+    - Latin America and the Caribbean: for the UN geographic subregion Latin America and the Caribbean
+    - Melanesia: for the UN geographic subregion Melanesia
+    - Micronesia: for the UN geographic subregion Micronesia
+    - Northern Africa: for the UN geographic subregion Northern Africa
+    - Northern America: for the UN geographic subregion Northern America
+    - Northern Europe: for the UN geographic subregion Northern Europe
+    - Polynesia: for the UN geographic subregion Polynesia
+    - South-eastern Asia: for the UN geographic subregion South-eastern Asia
+    - Southern Asia: for the UN geographic subregion Southern Asia
+    - Southern Europe: for the UN geographic subregion Southern Europe
+    - Sub-Saharan Africa: for the UN geographic subregion Sub-Saharan Africa
+    - Western Asia: for the UN geographic subregion Western Asia
+    - Western Europe: for the UN geographic subregion Western Europe
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#dt-regionorsubregion
+    """
+    AFRICA = 'Africa'
+    AMERICAS = 'Americas'
+    ASIA = 'Asia'
+    EUROPE = 'Europe'
+    OCEANIA = 'Oceania'
+    AUSTRALIA_AND_NEW_ZEALAND = 'Australia and New Zealand'
+    CENTRAL_ASIA = 'Central Asia'
+    EASTERN_ASIA = 'Eastern Asia'
+    EASTERN_EUROPE = 'Eastern Europe'
+    LATIN_AMERICA_AND_THE_CARIBBEAN = 'Latin America and the Caribbean'
+    MELANESIA = 'Melanesia'
+    MICRONESIA = 'Micronesia'
+    NORTHERN_AFRICA = 'Northern Africa'
+    NORTHERN_AMERICA = 'Northern America'
+    NORTHERN_EUROPE = 'Northern Europe'
+    POLYNESIA = 'Polynesia'
+    SOUTH_EASTERN_ASIA = 'South-eastern Asia'
+    SOUTHERN_ASIA = 'Southern Asia'
+    SOUTHERN_EUROPE = 'Southern Europe'
+    SUB_SAHARAN_AFRICA = 'Sub-Saharan Africa'
+    WESTERN_ASIA = 'Western Asia'
+    WESTERN_EUROPE = 'Western Europe'
 
 
 class CarbonFootprint(BaseModel):
-    declaredUnit: str
-    unitaryProductAmount: int
-    pCfExcludingBiogenic: float
-    pCfIncludingBiogenic: float
-    fossilGhgEmissions: int
-    fossilCarbonContent: int
-    biogenicCarbonContent: int
-    characterizationFactors: str
-    crossSectoralStandardsUsed: List[str]
-    productOrSectorSpecificRules: List[str]
-    boundaryProcessesDescription: str
-    referencePeriodStart: str = Field(default_factory=lambda: datetime.now(timezone.utc).isoformat())
-    referencePeriodEnd: str = Field(default_factory=lambda: datetime.now(timezone.utc).isoformat())
-    exemptedEmissionsPercent: float
-    exemptedEmissionsDescription: str
-    packagingEmissionsIncluded: bool
+    model_config = ConfigDict(
+        use_enum_values=True
+    )
+    declaredUnit: DeclaredUnit = Field(
+        ...,
+        description="DeclaredUnit is the enumeration of accepted declared units with values"
+    )
+    unitaryProductAmount: PositiveFloat = Field(
+        ...,
+        description='Amount of declared units in the product (must be greater than 0).',
+    )
+    pCfExcludingBiogenic: confloat(ge=0.0) = Field(
+        ...,
+        description='Product carbon footprint excluding biogenic CO2 emissions (kgCO2e/declaredUnit).',
+    )
+    pCfIncludingBiogenic: Optional[float] = Field(
+        None,
+        description='Product carbon footprint including all biogenic emissions (kgCO2e/declaredUnit).',
+    )
+    fossilGhgEmissions: confloat(ge=0.0) = Field(
+        ..., description='Emissions from fossil sources (kgCO2e/declaredUnit).'
+    )
+    fossilCarbonContent: confloat(ge=0.0) = Field(
+        ..., description='Fossil carbon content of the product (kgC/declaredUnit).'
+    )
+    biogenicCarbonContent: confloat(ge=0.0) = Field(
+        ..., description='Biogenic carbon content of the product (kgC/declaredUnit).'
+    )
+    dLucGhgEmissions: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description='Emissions from recent direct land use change (kgCO2e/declaredUnit).',
+    )
+    landManagementGhgEmissions: Optional[float] = Field(
+        None,
+        description='Emissions/removals from land management (kgCO2e/declaredUnit).',
+    )
+    otherBiogenicGhgEmissions: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description='Other biogenic GHG emissions not in dLUC or landManagement (kgCO2e/declaredUnit).',
+    )
+    iLucGhgEmissions: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description='Emissions from recent indirect land use change (kgCO2e/declaredUnit).',
+    )
+    biogenicCarbonWithdrawal: Optional[confloat(le=0.0)] = Field(
+        None,
+        description='Biogenic carbon contained in the product converted to kgCO2e (must be less than or equal to zero).',
+    )
+    aircraftGhgEmissions: Optional[confloat(ge=0.0)] = Field(
+        None,
+        description='GHG emissions from aircraft usage for product transport (kgCO2e/declaredUnit).',
+    )
+    characterizationFactors: CharacterizationFactors = Field(
+        ...,
+        description='The IPCC Global Warming Potential characterization factors used.',
+    )
+    crossSectoralStandardsUsed: List[str] = Field(
+        ...,
+        description='Cross-sectoral standards applied for calculating GHG emissions.',
+    )
+    productOrSectorSpecificRules: Optional[List[str]] = Field(
+        None,
+        description='Product- or sector-specific rules for calculating GHG emissions.',
+    )
+    biogenicAccountingMethodology: Optional[BiogenicAccountingMethodology] = Field(
+        None,
+        description='The standard followed to account for biogenic emissions and removals.',
+    )
+    boundaryProcessesDescription: str = Field(
+        ..., description='Description of processes included in each lifecycle stage.'
+    )
+    referencePeriodStart: AwareDatetime = Field(
+        ..., description='Start of the time boundary for the PCF (ISO 8601)'
+    )
+    referencePeriodEnd: AwareDatetime = Field(
+        ..., description='End of the time boundary for the PCF (ISO 8601)'
+    )
+    geographyCountrySubdivision: Optional[str] = Field(
+        None, description='ISO 3166-2 Subdivision Code if applicable.'
+    )
+    geographyCountry: Optional[constr(pattern=r'^[A-Z]{2}$')] = Field(
+        None, description='ISO 3166 Country Code if applicable.'
+    )
+    geographyRegionOrSubregion: Optional[RegionOrSubregion] = None
+    secondaryEmissionFactorSources: Optional[List[str]] = Field(
+        None, description='List of emission factor sources for secondary data, if used.'
+    )
+    exemptedEmissionsPercent: confloat(ge=0.0, le=5.0) = Field(
+        ..., description='Percentage of emissions exempted from PCF (0 to 5).'
+    )
+    exemptedEmissionsDescription: str = Field(
+        ..., description='Reasoning for excluding emissions.'
+    )
+    packagingEmissionsIncluded: bool = Field(
+        ..., description='Indicates if packaging emissions are in the PCF.'
+    )
+    packagingGhgEmissions: Optional[confloat(ge=0.0)] = Field(
+        None, description='Emissions from product packaging (kgCO2e/declaredUnit).'
+    )
+    allocationRulesDescription: Optional[str] = Field(
+        None, description='Description of any allocation rules applied.'
+    )
+    uncertaintyAssessmentDescription: Optional[str] = Field(
+        None, description='Description of the uncertainty assessment.'
+    )
+    primaryDataShare: Optional[confloat(ge=0.0, le=100.0)] = Field(
+        None, description='The share of primary data in percent.'
+    )
+    dqi: Optional[Dict[str, Any]] = Field(
+        None, description='Data Quality Indicators, if applicable.'
+    )
+    assurance: Optional[Dict[str, Any]] = Field(
+        None, description='Assurance information, if applicable.'
+    )
 
-    @field_validator('exemptedEmissionsPercent')
-    @classmethod
-    def check_percent(cls, v):
-        assert 0 <= v <= 5, "Value must be a decimal number between 0.0 and 5."
-        return v
-    
+    @field_serializer('referencePeriodStart')
+    def serialize_reference_period_start(self, referencePeriodStart: AwareDatetime, _info):
+        return referencePeriodStart.isoformat()
+
+    @field_serializer('referencePeriodEnd')
+    def serialize_reference_period_end(self, referencePeriodEnd: AwareDatetime, _info):
+        return referencePeriodEnd.isoformat()
+
--- tests/test_schemas/test_carbon_footprint_schema.py
+++ tests/test_schemas/test_carbon_footprint_schema.py
@@ -1,29 +1,50 @@
-from datetime import datetime
+from datetime import datetime, timezone
+import json
 
 import pytest
 
-from schemas.carbon_footprint import CarbonFootprint
+from schemas.carbon_footprint import (
+    CarbonFootprint, RegionOrSubregion, DeclaredUnit, BiogenicAccountingMethodology,
+    CharacterizationFactors
+)
 
 
 @pytest.fixture(scope="function")
 def valid_carbon_footprint_data():
     data = {
-        "declaredUnit": "kgCO2e/kg",
+        "declaredUnit": "kilogram",
         "unitaryProductAmount": 100,
         "pCfExcludingBiogenic": 10,
         "pCfIncludingBiogenic": 12,
         "fossilGhgEmissions": 8,
         "fossilCarbonContent": 5,
         "biogenicCarbonContent": 4,
-        "characterizationFactors": "IPCC 2013",
+        "dLucGhgEmissions": 2,
+        "landManagementGhgEmissions": 3,
+        "otherBiogenicGhgEmissions": 1,
+        "iLucGhgEmissions": 2,
+        "biogenicCarbonWithdrawal": -1,
+        "aircraftGhgEmissions": 0.5,
+        "characterizationFactors": "AR6",
         "crossSectoralStandardsUsed": ["PAS 2050"],
         "productOrSectorSpecificRules": ["CFS Guidance for XYZ Sector"],
+        "biogenicAccountingMethodology": "PEF",
         "boundaryProcessesDescription": "Description of boundary processes",
-        "referencePeriodStart": datetime(2023, 1, 1).isoformat(),
-        "referencePeriodEnd": datetime(2023, 12, 31).isoformat(),
+        "referencePeriodStart": datetime(2023, 1, 1, tzinfo=timezone.utc).isoformat(),
+        "referencePeriodEnd": datetime(2023, 12, 31, tzinfo=timezone.utc).isoformat(),
+        "geographyCountrySubdivision": "AU",
+        "geographyCountry": "AU",
+        "geographyRegionOrSubregion": "Australia and New Zealand",
+        "secondaryEmissionFactorSources": ["Source 1", "Source 2"],
         "exemptedEmissionsPercent": 2.5,
         "exemptedEmissionsDescription": "Description of exempted emissions",
-        "packagingEmissionsIncluded": True
+        "packagingEmissionsIncluded": True,
+        "packagingGhgEmissions": 0.5,
+        "allocationRulesDescription": "Description of allocation rules",
+        "uncertaintyAssessmentDescription": "Description of uncertainty assessment",
+        "primaryDataShare": 50,
+        "dqi": {"key1": "value1", "key2": "value2"},
+        "assurance": {"key1": "value1", "key2": "value2"}
     }
 
     return data
@@ -32,7 +53,42 @@ def valid_carbon_footprint_data():
 def test_carbon_footprint_schema_validation(valid_carbon_footprint_data):
     """Test successful validation for valid carbon footprint data."""
     footprint = CarbonFootprint(**valid_carbon_footprint_data)
-    assert footprint.model_dump() == valid_carbon_footprint_data  # Assert all attributes are set correctly
+
+    json_footprint_data = json.loads(footprint.model_dump_json())
+
+    assert json_footprint_data["declaredUnit"] == "kilogram"
+    assert json_footprint_data["unitaryProductAmount"] == 100
+    assert json_footprint_data["pCfExcludingBiogenic"] == 10
+    assert json_footprint_data["pCfIncludingBiogenic"] == 12
+    assert json_footprint_data["fossilGhgEmissions"] == 8
+    assert json_footprint_data["fossilCarbonContent"] == 5
+    assert json_footprint_data["biogenicCarbonContent"] == 4
+    assert json_footprint_data["dLucGhgEmissions"] == 2
+    assert json_footprint_data["landManagementGhgEmissions"] == 3
+    assert json_footprint_data["otherBiogenicGhgEmissions"] == 1
+    assert json_footprint_data["iLucGhgEmissions"] == 2
+    assert json_footprint_data["biogenicCarbonWithdrawal"] == -1
+    assert json_footprint_data["aircraftGhgEmissions"] == 0.5
+    assert json_footprint_data["characterizationFactors"] == "AR6"
+    assert json_footprint_data["crossSectoralStandardsUsed"] == ["PAS 2050"]
+    assert json_footprint_data["productOrSectorSpecificRules"] == ["CFS Guidance for XYZ Sector"]
+    assert json_footprint_data["biogenicAccountingMethodology"] == "PEF"
+    assert json_footprint_data["boundaryProcessesDescription"] == "Description of boundary processes"
+    assert json_footprint_data["referencePeriodStart"] == "2023-01-01T00:00:00+00:00"
+    assert json_footprint_data["referencePeriodEnd"] == "2023-12-31T00:00:00+00:00"
+    assert json_footprint_data["geographyCountrySubdivision"] == "AU"
+    assert json_footprint_data["geographyCountry"] == "AU"
+    assert json_footprint_data["geographyRegionOrSubregion"] == "Australia and New Zealand"
+    assert json_footprint_data["secondaryEmissionFactorSources"] == ["Source 1", "Source 2"]
+    assert json_footprint_data["exemptedEmissionsPercent"] == 2.5
+    assert json_footprint_data["exemptedEmissionsDescription"] == "Description of exempted emissions"
+    assert json_footprint_data["packagingEmissionsIncluded"] == True
+    assert json_footprint_data["packagingGhgEmissions"] == 0.5
+    assert json_footprint_data["allocationRulesDescription"] == "Description of allocation rules"
+    assert json_footprint_data["uncertaintyAssessmentDescription"] == "Description of uncertainty assessment"
+    assert json_footprint_data["primaryDataShare"] == 50
+    assert json_footprint_data["dqi"] == {"key1": "value1", "key2": "value2"}
+    assert json_footprint_data["assurance"] == {"key1": "value1", "key2": "value2"}
 
 
 def test_carbon_footprint_missing_declared_unit(valid_carbon_footprint_data):
@@ -58,3 +114,56 @@ def test_carbon_footprint_invalid_data_types(valid_carbon_footprint_data):
     invalid_data["referencePeriodStart"] = "invalid_date"  # Should be a datetime
     with pytest.raises(ValueError):
         CarbonFootprint(**invalid_data)
+
+
+def test_region_or_subregion_values():
+    assert RegionOrSubregion.AFRICA == 'Africa'
+    assert RegionOrSubregion.AMERICAS == 'Americas'
+    assert RegionOrSubregion.ASIA == 'Asia'
+    assert RegionOrSubregion.EUROPE == 'Europe'
+    assert RegionOrSubregion.OCEANIA == 'Oceania'
+    assert RegionOrSubregion.AUSTRALIA_AND_NEW_ZEALAND == 'Australia and New Zealand'
+    assert RegionOrSubregion.CENTRAL_ASIA == 'Central Asia'
+    assert RegionOrSubregion.EASTERN_ASIA == 'Eastern Asia'
+    assert RegionOrSubregion.EASTERN_EUROPE == 'Eastern Europe'
+    assert RegionOrSubregion.LATIN_AMERICA_AND_THE_CARIBBEAN == 'Latin America and the Caribbean'
+    assert RegionOrSubregion.MELANESIA == 'Melanesia'
+    assert RegionOrSubregion.MICRONESIA == 'Micronesia'
+    assert RegionOrSubregion.NORTHERN_AFRICA == 'Northern Africa'
+    assert RegionOrSubregion.NORTHERN_AMERICA == 'Northern America'
+    assert RegionOrSubregion.NORTHERN_EUROPE == 'Northern Europe'
+    assert RegionOrSubregion.POLYNESIA == 'Polynesia'
+    assert RegionOrSubregion.SOUTH_EASTERN_ASIA == 'South-eastern Asia'
+    assert RegionOrSubregion.SOUTHERN_ASIA == 'Southern Asia'
+    assert RegionOrSubregion.SOUTHERN_EUROPE == 'Southern Europe'
+    assert RegionOrSubregion.SUB_SAHARAN_AFRICA == 'Sub-Saharan Africa'
+    assert RegionOrSubregion.WESTERN_ASIA == 'Western Asia'
+    assert RegionOrSubregion.WESTERN_EUROPE == 'Western Europe'
+
+
+def test_declared_unit_values():
+    assert DeclaredUnit.LITER == 'liter'
+    assert DeclaredUnit.KILOGRAM == 'kilogram'
+    assert DeclaredUnit.CUBIC_METER == 'cubic meter'
+    assert DeclaredUnit.KILOWATT_HOUR == 'kilowatt hour'
+    assert DeclaredUnit.MEGAJOULE == 'megajoule'
+    assert DeclaredUnit.TON_KILOMETER == 'ton kilometer'
+    assert DeclaredUnit.SQUARE_METER == 'square meter'
+
+
+def test_biogenic_accounting_methodology_values():
+    assert BiogenicAccountingMethodology.PEF.value == 'PEF'
+    assert BiogenicAccountingMethodology.ISO.value == 'ISO'
+    assert BiogenicAccountingMethodology.GHGP.value == 'GHGP'
+    assert BiogenicAccountingMethodology.QUANTIS.value == 'Quantis'
+
+    with pytest.raises(AttributeError):
+        assert BiogenicAccountingMethodology.Invalid
+
+
+def test_characterization_factors_values():
+    assert CharacterizationFactors.AR5 == 'AR5'
+    assert CharacterizationFactors.AR6 == 'AR6'
+
+    with pytest.raises(AttributeError):
+        assert CharacterizationFactors.Invalid
