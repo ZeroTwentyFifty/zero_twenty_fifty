d58760884072dd9b73e23bc829ae42d2014443c7
JohnVonNeumann <louiswillcock@gmail.com>
2024-03-01 14:05:16 +1100

    UPDATE carbon_footprint.py - add ProductOrSectorSpecificRuleOperator enum

--- schemas/carbon_footprint.py
+++ schemas/carbon_footprint.py
@@ -156,6 +156,25 @@ class CrossSectoralStandard(str, Enum):
     ISO_14044 = 'ISO Standard 14044'
 
 
+class ProductOrSectorSpecificRuleOperator(str, Enum):
+    """
+    A ProductOrSectorSpecificRuleOperator is the enumeration of Product Category Rule (PCR) operators. Valid values are:
+
+    - PEF: for EU / PEF Methodology PCRs
+    - EPD International: for PCRs authored or published by EPD International
+    - Other: for a PCR not published by the operators mentioned above
+
+    4.13.1. JSON Representation
+    Each value is encoded as a JSON String.
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#dt-productorsectorspecificruleoperator
+    """
+    PEF = 'PEF'
+    EPD_INTERNATIONAL = 'EPD International'
+    OTHER = 'Other'
+
+
 class EmissionFactorDS(BaseModel):
     """Represents an EmissionFactorDS, referencing emission factor databases.
 
