34ecbe773b90edb11c778fc80bf241c3975c75b1
JohnVonNeumann <louiswillcock@gmail.com>
2024-03-03 21:20:14 +1100

    UPDATE db/base.py - make sure all the models are picked up by SQLALchemy

--- db/base.py
+++ db/base.py
@@ -1,3 +1,4 @@
 from db.base_class import Base
 from db.models.user import User
 from db.models.product_footprint import ProductFootprint
+from db.models.carbon_footprint import CarbonFootprintModel, ProductOrSectorSpecificRuleModel
