6613613cea78c89cc29108e9a147872daecf05e1
JohnVonNeumann <louiswillcock@gmail.com>
2024-04-10 14:31:27 +1000

    UPDATE security.py - add some type hints for easier debugging

--- core/security.py
+++ core/security.py
@@ -29,7 +29,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
           with industry standards and reduce configuration complexity.
     """
 
-    to_encode = data.copy()
+    to_encode: dict = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
@@ -37,7 +37,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(
+    encoded_jwt: str = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
     )
     return encoded_jwt
