1a8f8096d3e83bcbc584cd2e0d6aa696cdce51d2
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-08 18:14:02 +1000

    UPDATE route_users.py - overhaul  route, add auth and check for superuser

--- tests/test_routes/test_users_route.py
+++ tests/test_routes/test_users_route.py
@@ -1,10 +1,26 @@
-def test_create_user(client):
-    data = {
-        "username": "testuser",
-        "email": "testuser@nofoobar.com",
-        "password": "testing",
-    }
-    response = client.post(url="/users/", json=data)
+def test_create_user_by_superuser(client, superuser_auth_header):
+    data = {"username": "testuser", "email": "testuser@example.com", "password": "testpassword"}
+    response = client.post("/users/", headers=superuser_auth_header, json=data)
     assert response.status_code == 200
-    assert response.json()["email"] == "testuser@nofoobar.com"
-    assert response.json()["is_active"] is True
+    assert response.json()["username"] == "testuser"
+    assert response.json()["email"] == "testuser@example.com"
+
+
+def test_create_user_no_auth(client):
+    data = {"username": "testuser", "email": "testuser@example.com", "password": "testpassword"}
+    response = client.post("/users/", json=data)
+    assert response.status_code == 400
+
+
+def test_create_user_by_normal_user(client, auth_header):
+    data = {"username": "testuser", "email": "testuser@example.com", "password": "testpassword"}
+    response = client.post("/users/", headers=auth_header, json=data)
+    assert response.status_code == 403
+    assert response.json()["code"] == "AccessDenied"
+
+
+def test_create_user_with_invalid_data(client, auth_header):
+    data = {"username": "", "email": "testuser@example.com", "password": "testpassword"}
+    response = client.post("/users/", headers=auth_header, json=data)
+    assert response.status_code == 403
+    assert response.json()["code"] == "AccessDenied"
